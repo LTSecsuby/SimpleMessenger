3e70c8961006c21cb86ef8b224bb6a199185f9ce
Evgeniy198 <Busygin1987@gmail.com>
2020-06-21 12:33:37 +0500

    create commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
+import { NavbarContentComponent } from './navbar-content/navbar-content.component';
+import { NavbarUserComponent } from './navbar-user/navbar-user.component';
+import { ToolbarUserComponent } from './toolbar-user/toolbar-user.component';
+import { ToolbarContentComponent } from './toolbar-content/toolbar-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarMenuComponent,
+    NavbarContentComponent,
+    NavbarUserComponent,
+    ToolbarUserComponent,
+    ToolbarContentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/toolbar-content/toolbar-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar-content',
+  templateUrl: './toolbar-content.component.html',
+  styleUrls: ['./toolbar-content.component.css']
+})
+export class ToolbarContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
